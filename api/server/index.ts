00942c9d9d80f744249df4449dadccdaf907538c
djonko <prevostdjonko@gmail.com>
2021-11-28 17:32:33 -0500

    fix

--- api/server/index.ts
+++ api/server/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 import cors, { CorsOptions } from "cors";
 import "reflect-metadata";
 import "process";
-import createSchema from "schema/index";
+import createSchema from "../schema";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 import createSession from "session";
 
